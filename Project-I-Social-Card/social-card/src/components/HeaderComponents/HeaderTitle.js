afbeb1fbad2ebaf12854998afa3a147df84d26cd
c87 <shaun.carmody@live.co.uk>
2019-02-11 23:05:17 +0000

    add date to header

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
 
-const HeaderTitle = () => 
-(
-    <h1 class="header-title">
-        Lambda School
-        <span className="handle">
-            @Lambda School
-        </span>
-        <span className="timestamp">
-        </span>
-    </h1>
-);
+const HeaderTitle = () => {
+    return (
+        <h1 class="header-title">
+            Lambda School
+            <span className="handle">
+                @Lambda School
+            </span>
+            <span className="timestamp">
+                {moment().format('DD MMM')}
+            </span>
+        </h1>
+    );
+}
+
 
 export default HeaderTitle;
