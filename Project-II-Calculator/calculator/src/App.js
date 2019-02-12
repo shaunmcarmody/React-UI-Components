8148716c41cd5f39b0e918bcab693a34b290539c
c87 <shaun.carmody@live.co.uk>
2019-02-12 22:34:53 +0000

    change App component to Class component

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -4,34 +4,43 @@ import ActionButton from './components/ButtonComponents/ActionButton';
 import NumberButton from './components/ButtonComponents/NumberButton';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
-const App = () => {
-  return (
-    <div className="calculator">
-      <CalculatorDisplay displayStyle="display" text="0"/>
-      <div className="flex">
-        <ActionButton buttonStyle="clear" text="clear" />
-        <NumberButton buttonStyle="symbol" text="÷"/>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      total: 0
+    }
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <CalculatorDisplay displayStyle="display" text={this.state.total}/>
+        <div className="flex">
+          <ActionButton buttonStyle="clear" text="clear" />
+          <NumberButton buttonStyle="symbol" text="÷"/>
+        </div>
+        <div className="flex wrap">
+          <NumberButton buttonStyle="number" text="7"/>
+          <NumberButton buttonStyle="number" text="8"/>
+          <NumberButton buttonStyle="number" text="9"/>
+          <NumberButton buttonStyle="symbol" text="×"/>
+          <NumberButton buttonStyle="number" text="4"/>
+          <NumberButton buttonStyle="number" text="5"/>
+          <NumberButton buttonStyle="number" text="6"/>
+          <NumberButton buttonStyle="symbol" text="-"/>
+          <NumberButton buttonStyle="number" text="1"/>
+          <NumberButton buttonStyle="number" text="2"/>
+          <NumberButton buttonStyle="number" text="3"/>
+          <NumberButton buttonStyle="symbol" text="+"/>
+        </div>
+        <div className="flex">
+        <ActionButton buttonStyle="big zero" text="0" />
+          <NumberButton buttonStyle="symbol" text="="/>
+        </div>
       </div>
-      <div className="flex wrap">
-        <NumberButton buttonStyle="number" text="7"/>
-        <NumberButton buttonStyle="number" text="8"/>
-        <NumberButton buttonStyle="number" text="9"/>
-        <NumberButton buttonStyle="symbol" text="×"/>
-        <NumberButton buttonStyle="number" text="4"/>
-        <NumberButton buttonStyle="number" text="5"/>
-        <NumberButton buttonStyle="number" text="6"/>
-        <NumberButton buttonStyle="symbol" text="-"/>
-        <NumberButton buttonStyle="number" text="1"/>
-        <NumberButton buttonStyle="number" text="2"/>
-        <NumberButton buttonStyle="number" text="3"/>
-        <NumberButton buttonStyle="symbol" text="+"/>
-      </div>
-      <div className="flex">
-      <ActionButton buttonStyle="big zero" text="0" />
-        <NumberButton buttonStyle="symbol" text="="/>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
